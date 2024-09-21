00eafa207167f0c9107fe9d5be0a459d79dd3715
jahidul2004 <islamjahiduljihad@gmail.com>
2024-09-21 20:42:28 +0600

    To Do Project Updated.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
+let initial = 0;
+
 document.getElementById("add").addEventListener("click", function () {
     let text = document.getElementById("input-text").value;
-    let li = document.createElement("div");
+    let div = document.createElement("div");
 
     if (text === "") {
         alert("Please enter a task");
     } else {
-        li.innerText = text;
+        div.innerHTML = `
+        <p class="text-xl font-extrabold">${text}</p>
+        `;
+        div.classList.add("p-3", "rounded-lg", "my-3");
 
-        li.classList.add("list");
+        if (initial == 0) {
+            div.classList.add("bg-success");
+            initial++;
+        } else if (initial == 1) {
+            div.classList.add("bg-error");
+            initial++;
+        } else if (initial == 2) {
+            div.classList.add("bg-warning");
+            initial++;
+        } else if (initial == 3) {
+            div.classList.add("bg-info");
+            initial++;
+        } else if (initial == 4) {
+            div.classList.add("bg-secondary");
+            initial++;
+        } else if (initial == 5) {
+            div.classList.add("bg-accent");
+            initial = 0;
+        }
 
-        document.getElementById("list-container").appendChild(li);
+        document.getElementById("list-container").appendChild(div);
         document.getElementById("input-text").value = "";
     }
 });
